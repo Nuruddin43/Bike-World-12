e4382f62e46019b77122800592aea0224de4f31d
Nur Uddin <nuruddin46bd@gmail.com>
2022-05-28 12:38:56 +0600

    payment set completed

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,13 +1,20 @@
+import { Elements } from "@stripe/react-stripe-js"
+import { loadStripe } from "@stripe/stripe-js"
 import React from "react"
 
 import { useQuery } from "react-query"
 import { useParams } from "react-router-dom"
 import Loading from "../Shared/Loading"
+import CheckoutForm from "./CheckoutForm"
+
+const stripePromise = loadStripe(
+  "pk_test_51L1GfrL5x1PqRDImFqrYR6vQ6uE81qtIiI5ZqnEiekrf2BRzqWhGPkTgNvPdoiHOicNBFl94wwjvDkuel2PvgZTy00qd8VCA6p"
+)
 
 const Payment = () => {
   const { id } = useParams()
   const url = `http://localhost:5000/order/${id}`
-  const { data: order, isLoading } = useQuery(["booking", id], () =>
+  const { data: order, isLoading } = useQuery(["order", id], () =>
     fetch(url, {
       method: "GET",
     }).then((res) => res.json())
@@ -29,7 +36,11 @@ const Payment = () => {
         </div>
       </div>
       <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
-        <div class="card-body"></div>
+        <div class="card-body">
+          <Elements stripe={stripePromise}>
+            <CheckoutForm order={order} />
+          </Elements>
+        </div>
       </div>
     </div>
   )
