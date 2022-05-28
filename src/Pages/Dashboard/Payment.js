b443d97c8840786d18b7ca23f2650a8632716139
Nur Uddin <nuruddin46bd@gmail.com>
2022-05-28 07:31:03 +0600

    payment get started

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,11 +1,36 @@
 import React from "react"
+
+import { useQuery } from "react-query"
 import { useParams } from "react-router-dom"
+import Loading from "../Shared/Loading"
 
 const Payment = () => {
   const { id } = useParams()
+  const url = `http://localhost:5000/order/${id}`
+  const { data: order, isLoading } = useQuery(["booking", id], () =>
+    fetch(url, {
+      method: "GET",
+    }).then((res) => res.json())
+  )
+  if (isLoading) {
+    return <Loading></Loading>
+  }
   return (
     <div>
-      <h2 className="text-2xl text-purple-500">Pay: {id}</h2>
+      <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12 ">
+        <div class="card-body ">
+          <p className="text-2xl text-success">Hello, {order.name}</p>
+          <h2 class="text-3xl">Please Pay for {order.product}</h2>
+          <p>
+            Delivered pick point at
+            <span className="text-purple-500 pl-2">{order.address}</span>
+          </p>
+          <p>Please Pay ${order.price}</p>
+        </div>
+      </div>
+      <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
+        <div class="card-body"></div>
+      </div>
     </div>
   )
 }
