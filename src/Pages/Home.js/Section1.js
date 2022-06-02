05d5b89f994cdb6117df7be3ce2074aa45f9b3e2
Nur Uddin <nuruddin46bd@gmail.com>
2022-06-02 22:25:13 +0600

    Latest

--- src/Pages/Home.js/Section1.js
+++ src/Pages/Home.js/Section1.js
@@ -7,7 +7,7 @@ const Section1 = () => {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <img src={byc1} className="max-w-lg rounded-lg shadow-2xl" alt="" />
         <div>
-          <h1 className="text-5xl font-bold">About our Company!!</h1>
+          <h1 className="text-5xl font-bold">About our Company!!!</h1>
           <p className="py-6">
             CTGBIKE was established in 2003 and provides bicycle OEM services.We
             have 19 years of bicycle manufacturing and exporting experience.The
